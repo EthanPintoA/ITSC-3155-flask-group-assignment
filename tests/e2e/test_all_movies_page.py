f76ee95794e8e484704eda845c9f7d4ea31f2f00
Caden Lalley <cadenlalley@gmail.com>
2022-03-14 19:08:56 -0400

    Finished End-to-end test

--- tests/e2e/test_all_movies_page.py
+++ tests/e2e/test_all_movies_page.py
@@ -1,7 +1,25 @@
 from flask.testing import FlaskClient
+from src.repositories.movie_repository import movie_repository_singleton
 from app import app
 
 def test_all_movies_page():
-    testing_app = app.test_client()
-
     
+
+    movie_repository_singleton.create_movie("Batman", "Caden", 4)
+    movie_repository_singleton.create_movie("Superman", "William", 5)
+    movie_repository_singleton.create_movie("Hulk", "Josh", 3)
+
+    testing_app = app.test_client()
+    response = testing_app.get('/movies')
+    
+    assert b"<td>Batman</td>" in response.data
+    assert b"<td>Caden</td>" in response.data
+    assert b"<td>4</td>" in response.data
+
+    assert b"<td>Superman</td>" in response.data
+    assert b"<td>William</td>" in response.data
+    assert b"<td>5</td>" in response.data
+
+    assert b"<td>Hulk</td>" in response.data
+    assert b"<td>Josh</td>" in response.data
+    assert b"<td>3</td>" in response.data
